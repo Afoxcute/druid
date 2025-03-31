56b4611ebfd6a0e511eb673f7789cab85307f329
Afoxcute <jwavolabilove16@gmail.com>
2025-03-31 14:17:46 +0000

    smart-wallet fix

--- src/lib/client-helpers.ts
+++ src/lib/client-helpers.ts
@@ -6,7 +6,7 @@ import { Buffer } from "buffer";
 import { basicNodeSigner } from "@stellar/stellar-sdk/minimal/contract";
 import { Server } from "@stellar/stellar-sdk/minimal/rpc";
 
-export const rpc = new Server(env.NEXT_PUBLIC_RPC_URL);
+export const rpc = new Server(env.RPC_URL);
 
 export const mockPubkey = StrKey.encodeEd25519PublicKey(Buffer.alloc(32))
 export const mockSource = new Account(mockPubkey, '0')
